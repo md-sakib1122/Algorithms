75e2d2da858f87b1a6685b92ff2882fb6145330e
Md Sakib <125847210+md-sakib1122@users.noreply.github.com>
2024-10-21 09:24:39 +0600

    Update BinaryExponentiations.cpp
    Its take 2 number 'a' and 'n' and return a^n . approach:alws make pair of 'a' and multiply them. So the n become half every time when we make pair....and continue the process, Time complexity log(n)

--- BinaryExponentiations.cpp
+++ BinaryExponentiations.cpp
@@ -1,3 +1,6 @@
+//its take 2 number 'a' and 'n' and return a^n . approach:alws make pair of 'a' and multiply them. 
+//So the n become half every time when we make pair....and continue the process, Time complexity log(n)
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
